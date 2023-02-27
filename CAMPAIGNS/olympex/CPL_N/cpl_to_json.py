f29d60de6cefd3ecabc1bbd19237148019d6f171
sanjog <st0128@uah.edu>
2023-02-27 10:26:43 -0600

    after removing unnecessary filters and corrections on cpl dataset.

--- CAMPAIGNS/olympex/CPL_N/cpl_to_json.py
+++ CAMPAIGNS/olympex/CPL_N/cpl_to_json.py
@@ -39,53 +39,13 @@ def ingest(folder, file, s3bucket):
     z_location = root.create_dataset('location', shape=(0, 3), chunks=(chunk, None), dtype=np.float32)
     z_time = root.create_dataset('time', shape=(0), chunks=(chunk), dtype=np.int32)
     z_vars = root.create_group('value')
-    z_ref = z_vars.create_dataset('ref', shape=(0), chunks=(chunk), dtype=np.float32)
+    z_ref = z_vars.create_dataset('atb', shape=(0), chunks=(chunk), dtype=np.float32)
     n_time = np.array([], dtype=np.int64)
 
-    date = file.split("_")[2]
-    # base_time = np.datetime64('{}-{}-{}'.format(date[:4], date[4:6], date[6:]))
+    date = file.split("_")[5].split(".")[0]
+    base_time = np.datetime64('{}-{}-{}'.format(date[:4], date[4:6], date[6:]))
 
     print("Accessing file from S3 ", file)
-
-    # read from s3 url (file) in s3 bucket.
-    # fileObj = CRSaccess(file, s3bucket=s3bucket)
-
-##########################################################################
-
-    # # open dataset hdf5 format, so use 
-    # with xr.open_dataset(fileObj, decode_cf=False) as ds:
-    #     # added for time correction for over 24h UTC
-    #     hr = add24hr(ds['timed'].values)
-    #     delta = (hr * 3600).astype('timedelta64[s]') + base_time
-    #     # time correction end
-        
-    #     # data columns extract
-    #     ref = ds["zku"].values #CRS radar reflectivity
-    #     rad_range = ds["range"].values
-        
-    #     lat = ds['lat'].values
-    #     lon = ds['lon'].values
-    #     alt = ds['altitude'].values # altitude of aircraft in meters
-    #     roll = ds["roll"].values
-    #     pitch = ds["pitch"].values
-    #     head = ds["head"].values
-    # num_col = ref.shape[0] # number of cols
-    # num_row = ref.shape[1] # number of rows
-    
-    # # data frame formation
-    # delta = np.repeat(delta, num_row)
-    # lon = np.repeat(lon, num_row)
-    # lat = np.repeat(lat, num_row)
-    # alt = np.repeat(alt, num_row)
-    # roll = np.repeat(roll * to_rad, num_row)
-    # pitch = np.repeat(pitch * to_rad, num_row)
-    # head = np.repeat(head * to_rad, num_row)
-    # rad_range = np.tile(rad_range, num_col)
-    # ref = ref.flatten()
-
-    
-    
-    ###########################################################################
     
     fs = s3fs.S3FileSystem(anon=False)
     with fs.open(file) as cplfile:
@@ -94,23 +54,25 @@ def ingest(folder, file, s3bucket):
             # atb1064 = f1['ATB_1064'][()]
             # AltBin = f1['Bin_Alt'][()] * 1000    #[km] ==> [m]
             
-            ref = f1['ATB_1064'][()]
-            rad_range = f1['Bin_Alt'][()] * 1000    #[km] ==> [m]
+            atb = f1['ATB_1064'][()]
+            # rad_range = f1['Bin_Alt'][()] * 1000    #[km] ==> [m]
             
             lon  = f1['Longitude'][()]
             lat  = f1['Latitude'][()]
             alt  = f1['Plane_Alt'][()] * 1000   #[km] ==> [m]
+            
             roll = f1['Plane_Roll'][()] * to_rad
             head = f1['Plane_Heading'][()] * to_rad
             pitch = f1['Plane_Pitch'][()] * to_rad
             
-            delta = [h*3600+m*60+s for (h,m,s) in 
-                    zip(f1['Hour'][()], f1['Minute'][()], f1['Second'][()])]
+            # if over 24 hour fix yet not applied
+            delta = [(base_time + (h*3600+m*60+s).astype('timedelta64[s]')) for (h,m,s) in 
+                    zip(f1['Hour'][()], f1['Minute'][()], f1['Second'][()])] #delta is in seconds
 
-    # some corrections
-    delta = np.array(delta)
-    delta[delta < delta[0]] = delta[delta < delta[0]] + 86400 #account for time over 00Z
 
+    num_col = atb.shape[0] # number of cols, say 7903
+    num_row = atb.shape[1] # number of rows, say 757
+    
     # Alt1D = syncDim(Alt1D, Sec1D)
     # roll1D = syncDim(roll1D, Sec1D)
     # head1D = syncDim(head1D, Sec1D)
@@ -122,22 +84,18 @@ def ingest(folder, file, s3bucket):
     # atb1064.shape
 
     # dataFrame formation
-    CPL = pd.DataFrame()
-    ncol, nrow = ref.shape
+   
+    delta = np.repeat(delta, num_row)
+    lon = np.repeat(lon, num_row)
+    lat = np.repeat(lat, num_row)
+    alt = np.repeat(alt, num_row)
+    roll = np.repeat(roll * to_rad, num_row)
+    pitch = np.repeat(pitch * to_rad, num_row)
+    head = np.repeat(head * to_rad, num_row)
     
-    ref = ref.flatten()
-    rad_range = np.tile(rad_range, ncol)
-    
-    lon = np.repeat(lon, nrow)
-    lat = np.repeat(lat, nrow)
-    alt = np.repeat(alt, nrow)
-    roll  = np.repeat(roll, nrow)
-    head  = np.repeat(head, nrow)
-    pitch = np.repeat(pitch, nrow)
-    delta = np.repeat(delta, nrow)
+    atb = atb.flatten()
     
     ###########################################################################
-    
     
     
     
@@ -159,14 +117,15 @@ def ingest(folder, file, s3bucket):
     # time correction.
     time = (delta - np.datetime64('1970-01-01')).astype('timedelta64[s]').astype(np.int64)
 
-    x, y, z = down_vector(roll, pitch, head)
-    x = np.multiply(x, np.divide(rad_range, 111000 * np.cos(lat * to_rad)))
-    y = np.multiply(y, np.divide(rad_range, 111000))
-    z = np.multiply(z, rad_range)
+    # !!! no lon alt lat correction for now.
+    # x, y, z = down_vector(roll, pitch, head)
+    # x = np.multiply(x, np.divide(rad_range, 111000 * np.cos(lat * to_rad)))
+    # y = np.multiply(y, np.divide(rad_range, 111000))
+    # z = np.multiply(z, rad_range)
 
-    lon = np.add(-x, lon)
-    lat = np.add(-y, lat)
-    alt = np.add(z, alt)
+    # lon = np.add(-x, lon)
+    # lat = np.add(-y, lat)
+    # alt = np.add(z, alt)
 
     # sort data by time
     sort_idx = np.argsort(time)
@@ -174,20 +133,21 @@ def ingest(folder, file, s3bucket):
     lon = lon[sort_idx]
     lat = lat[sort_idx]
     alt = alt[sort_idx]
-    ref = ref[sort_idx]
+    atb = atb[sort_idx]
     time = time[sort_idx]
 
     # remove nan and infinite using mask ???
-    mask = np.logical_and(np.isfinite(ref), alt > 0)
-    lon = lon[mask]
-    lat = lat[mask]
-    alt = alt[mask]
-    ref = ref[mask]
-    time = time[mask]
+    # mask = np.logical_and(np.isfinite(atb), alt > 0)
+    # mask = np.logical_and(np.isfinite(atb), alt)
+    # lon = lon[mask]
+    # lat = lat[mask]
+    # alt = alt[mask]
+    # atb = atb[mask]
+    # time = time[mask]
 
     # Now populate (append) the empty rows with modified data.
     z_location.append(np.stack([lon, lat, alt], axis=-1))
-    z_ref.append(ref)
+    z_ref.append(atb)
     n_time = np.append(n_time, time)
 
     idx = np.arange(0, n_time.size, chunk)
@@ -229,9 +189,9 @@ def data_pre_process(bucket_name, field_campaign, input_data_dir, output_data_di
         keys.append(obj.key)
 
     result = keys
-    for s3_raw_file_key in [result[0]]:
+    for s3_raw_file_key in [result[13]]:
         # SOURCE DIR.
-        sdate = s3_raw_file_key.split('_')[2]
+        sdate = s3_raw_file_key.split("_")[5].split(".")[0]
         print(f'processing CRS file {s3_raw_file_key}')
 
         # CREATE A LOCAL DIR TO HOLD RAW DATA AND CONVERTED DATA
@@ -244,16 +204,16 @@ def data_pre_process(bucket_name, field_campaign, input_data_dir, output_data_di
         src_s3_path = f"s3://{bucket_name}/{s3_raw_file_key}"
         ingest(folder, src_s3_path, bucket_name)
         # CONVERT ZARR FILE INTO 3D TILESET JSON.
-        generate_point_cloud("ref",  0,  1000000000000, folder, point_cloud_folder)
-
+        generate_point_cloud("atb",  0,  1000000000000, folder, point_cloud_folder)
         # UPLOAD CONVERTED FILES.
         files = os.listdir(point_cloud_folder)
         s3 = boto3.client('s3')
-        # for file in files:
-        #     fname = os.path.join(point_cloud_folder, file) # SOURCE
-        #     s3name = f"{field_campaign}/{output_data_dir}/cpl/{sdate}/{file}" # DESTINATION
-        #     print(f"uploaded to {s3name}.")
-        #     upload_to_s3(fname, bucket_name, s3_name=s3name)
+        for file in files:
+            fname = os.path.join(point_cloud_folder, file) # SOURCE
+            s3name = f"{field_campaign}/{output_data_dir}/cpl/{sdate}/{file}" # DESTINATION
+            print(f"uploaded to {s3name}.")
+            upload_to_s3(fname, bucket_name, s3_name=s3name)
+        return
 
 
 def cpl():
