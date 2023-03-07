6e145e638f0c3d25f2417f8a1c4a1f787567d9f1
sanjog <st0128@uah.edu>
2023-03-07 13:16:37 -0600

    nexrad czml writer skeleton added

--- CAMPAIGNS/olympex/NEXRAD/img_to_czml.py
+++ CAMPAIGNS/olympex/NEXRAD/img_to_czml.py
@@ -1,68 +1,28 @@
-# A sample czml that will display the nexrad time dynamic imagery data in cesium
-czml = [
-    {
-      "id": "document",
-      "name": "CZML Geometries Rectangle",
-      "version": "1.0",
-      "clock": {
-        "interval": "2015-09-22T22:28:00Z/2015-09-22T23:58:00Z",
-        "currentTime": "2015-09-22T22:28:00Z",
-        "multiplier": 20,
-      },
-    },
-    {
-      "id": "textureRectangle1",
-      "name": "rectangle with image, above surface",
-      "availability": "2015-09-22T22:29:00Z/2015-09-22T22:38:00Z",
-      "rectangle": {
-        "coordinates": {
-          "wsenDegrees": [-123.197, 48.735, -121.812, 49.653],
-        },
-        "height": 0,
-        "fill": True,
-        "material": {
-          "image": {
-            "image": { "uri": "https://ghrc-fcx-field-campaigns-szg.s3.amazonaws.com/Olympex/instrument-raw-data/nexrad/katx/2015-09-22/olympex_Level2_KATX_20150922_2229_ELEV_01.png" },
-            "color": {
-              "rgba": [255, 255, 255, 128],
-            },
-          },
-        },
-      },
-    },
-    {
-      "id": "textureRectangle2",
-      "name": "rectangle with image, above surface",
-      "availability": "2015-09-22T22:38:00Z/2015-09-22T22:48:00Z",
-      "rectangle": {
-        "coordinates": {
-          "wsenDegrees": [-123.197, 48.735, -121.812, 49.653],
-        },
-        "height": 0,
-        "fill": True,
-        "material": {
-          "image": {
-            "image": { "uri": "https://ghrc-fcx-field-campaigns-szg.s3.amazonaws.com/Olympex/instrument-raw-data/nexrad/katx/2015-09-22/olympex_Level2_KATX_20150922_2238_ELEV_01.png", },
-            "color": {
-              "rgba": [255, 255, 255, 128],
-            },
-          },
-        },
-      },
-    },
-  ]
+import boto3
+import os
+
+def data_pre_process(bucket_name, field_campaign, input_data_dir, output_data_dir, instrument_name):
+    s3_resource = boto3.resource('s3')
+    s3bucket = s3_resource.Bucket(bucket_name)    
+    keys = []
+    for obj in s3bucket.objects.filter(
+            Prefix=f"{field_campaign}/{input_data_dir}/{instrument_name}/olympex"):
+        keys.append(obj.key)
+
+    result = keys
+    for s3_raw_file_key in result:
+        # SOURCE DIR.
+        sdate = s3_raw_file_key.split('_')[3]
+        print(f'processing CRS file {s3_raw_file_key}')
+
+        # create a czml file.
+
+        # UPLOAD CONVERTED FILES.
+        output_czml = writer.get_string()
+        output_name = os.path.splitext(os.path.basename(s3_raw_file_key))[0]
+        output_name_wo_time = output_name.split("-")[0];
+        outfile = f"{field_campaign}/{output_data_dir}/{instrument_name}/{output_name_wo_time}.czml"
+        s3_client.put_object(Body=output_czml, Bucket=bucket_name, Key=outfile)
+        print(s3_raw_file_key+" conversion done.")
 
 
-"""
-The czml generation needs some variables where as some static properties:
-varying things:
-  - id: to identify each nexrad png image uniquely.
-  - availability: time span for the visibility of the rectangle. rectangle used to show the image
-         - availability is range. Determines when the rectangle is shown till when.
-         - start point is easy to find. i.e. the start date time is embedded in the filename
-         - but the end date time is not available.
-         - will have to depend on the next filename, to determine the end date time.
-  - coordinates: span for the rectangle to be shown.
-         - is actually a constant for a single ground station.
-  - image.uri: the url of the image to be shown in top of the rectangle.
-"""
---
+++ CAMPAIGNS/olympex/NEXRAD/nexrad_czml_writer.py
@@ -0,0 +1,90 @@
+import numpy as np
+from copy import deepcopy
+import json
+
+# A sample czml that will display the nexrad time dynamic imagery data in cesium
+sample_czml = [
+    {
+      "id": "document",
+      "name": "CZML Geometries Rectangle",
+      "version": "1.0",
+      "clock": {
+        "interval": "2015-09-22T22:28:00Z/2015-09-22T23:58:00Z",
+        "currentTime": "2015-09-22T22:28:00Z",
+        "multiplier": 20,
+      },
+    },
+    {
+      "id": "textureRectangle1",
+      "name": "rectangle with image, above surface",
+      "availability": "2015-09-22T22:29:00Z/2015-09-22T22:38:00Z",
+      "rectangle": {
+        "coordinates": {
+          "wsenDegrees": [-123.197, 48.735, -121.812, 49.653],
+        },
+        "height": 0,
+        "fill": True,
+        "material": {
+          "image": {
+            "image": { "uri": "https://ghrc-fcx-field-campaigns-szg.s3.amazonaws.com/Olympex/instrument-raw-data/nexrad/katx/2015-09-22/olympex_Level2_KATX_20150922_2229_ELEV_01.png" },
+            "color": {
+              "rgba": [255, 255, 255, 128],
+            },
+          },
+        },
+      },
+    },
+    {
+      "id": "textureRectangle2",
+      "name": "rectangle with image, above surface",
+      "availability": "2015-09-22T22:38:00Z/2015-09-22T22:48:00Z",
+      "rectangle": {
+        "coordinates": {
+          "wsenDegrees": [-123.197, 48.735, -121.812, 49.653],
+        },
+        "height": 0,
+        "fill": True,
+        "material": {
+          "image": {
+            "image": { "uri": "https://ghrc-fcx-field-campaigns-szg.s3.amazonaws.com/Olympex/instrument-raw-data/nexrad/katx/2015-09-22/olympex_Level2_KATX_20150922_2238_ELEV_01.png", },
+            "color": {
+              "rgba": [255, 255, 255, 128],
+            },
+          },
+        },
+      },
+    },
+  ]
+
+czml_head = sample_czml[0]
+model = sample_czml[1]
+
+"""
+The czml generation needs some variables where as some static properties:
+varying things:
+  - id: to identify each nexrad png image uniquely.
+  - availability: time span for the visibility of the rectangle. rectangle used to show the image
+         - availability is range. Determines when the rectangle is shown till when.
+         - start point is easy to find. i.e. the start date time is embedded in the filename
+         - but the end date time is not available.
+         - will have to depend on the next filename, to determine the end date time.
+  - coordinates: span for the rectangle to be shown.
+         - is actually a constant for a single ground station.
+  - image.uri: the url of the image to be shown in top of the rectangle.
+"""
+
+# class declaration
+
+class FlightTrackCzmlWriter:
+
+    def __init__(self, length):
+        self.model = deepcopy(model)
+
+    def set_time(self, time_window, time_steps):
+        pass
+    
+    def set_position(self, longitude, latitude, altitude):
+        pass
+
+    def get_string(self):
+        return json.dumps([czml_head, self.model])
