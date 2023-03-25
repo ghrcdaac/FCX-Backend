61c8abe40df61c8b85e90b624b8f41c68bdecf62
Sanjog Thapa <sanzog03@gmail.com>
2023-03-24 21:06:00 -0500

    uf to zarr conversion. zaar to 3d tile conversion done. visualization is successful. But code needs futher burnishings.

--- CAMPAIGNS/olympex/NPOL/npol_to_json.py
+++ CAMPAIGNS/olympex/NPOL/npol_to_json.py
@@ -11,6 +11,7 @@ import pandas as pd
 
 from npol_utils.point_cloud import generate_point_cloud
 from npol_utils.s3_updnload import upload_to_s3
+from uf_reader import Reader as UFReader
 
 
 # META needed for ingest
@@ -41,61 +42,89 @@ def ingest(folder, file, s3bucket):
     z_ref = z_vars.create_dataset('atb', shape=(0), chunks=(chunk), dtype=np.float32)
     n_time = np.array([], dtype=np.int64)
 
-    date = file.split("_")[5].split(".")[0]
-    base_time = np.datetime64('{}-{}-{}'.format(date[:4], date[4:6], date[6:]))
+    # date = file.split("_")[5].split(".")[0]
+    # base_time = np.datetime64('{}-{}-{}'.format(date[:4], date[4:6], date[6:]))
 
-    print("Accessing file from S3 ", file)
+    print("Accessing file to convert to zarr ")
     
-    fs = s3fs.S3FileSystem(anon=False)
-    with fs.open(file) as cplfile:
-        with h5py.File(cplfile, 'r') as f1:
-            atb = f1['ATB_1064'][()]            
-            lon  = f1['Longitude'][()]
-            lat  = f1['Latitude'][()]
-            alt  = f1['Plane_Alt'][()] * 1000   #[km] ==> [m]
+    # fs = s3fs.S3FileSystem(anon=False)
+    # with fs.open(file) as cplfile:
+    #     with h5py.File(cplfile, 'r') as f1:
+    #         atb = f1['ATB_1064'][()]            
+    #         lon  = f1['Longitude'][()]
+    #         lat  = f1['Latitude'][()]
+    #         alt  = f1['Plane_Alt'][()] * 1000   #[km] ==> [m]
 
-            # !!! if over 24 hour fix not applied
-            delta = [(base_time + (h*3600+m*60+s).astype('timedelta64[s]')) for (h,m,s) in 
-                    zip(f1['Hour'][()], f1['Minute'][()], f1['Second'][()])] #delta is in seconds
+    #         # !!! if over 24 hour fix not applied
+    #         delta = [(base_time + (h*3600+m*60+s).astype('timedelta64[s]')) for (h,m,s) in 
+    #                 zip(f1['Hour'][()], f1['Minute'][()], f1['Second'][()])] #delta is in seconds
+
+    ufr = UFReader()
+    uf_datas = ufr.read_data() # it will return a generator.
+
+    # using the generator, populate all the lon, lat, alt and atb values
+
+    atb = np.array([], dtype=np.int64)            
+    lon = np.array([], dtype=np.int64)
+    lat = np.array([], dtype=np.int64)
+    alt = np.array([], dtype=np.int64)
+    time = np.array([], dtype=np.int64)
+
+    for uf_data in uf_datas:
+        atb = np.append(atb, uf_data['CZ'])
+        lon = np.append(lon, uf_data['lon'])
+        lat = np.append(lat, uf_data['lat'])
+        alt = np.append(alt, uf_data['height'])
+        time = np.append(time, np.datetime64(uf_data['timestamp']).astype('timedelta64[s]').astype(np.int64))
+    
+    # using the values, create a zarr file and return it.
 
 
-    # num_col = atb.shape[0] # number of rows, say 7903
-    num_cols = atb.shape[1] # number of cols, say 757
+    # # !!! if over 24 hour fix not applied
+    # delta = [(base_time + (h*3600+m*60+s).astype('timedelta64[s]')) for (h,m,s) in 
+    #         zip(f1['Hour'][()], f1['Minute'][()], f1['Second'][()])] #delta is in seconds
 
-    # maintain data shape
-    delta = np.repeat(delta, num_cols)
-    lon = np.repeat(lon, num_cols)
-    lat = np.repeat(lat, num_cols)
-    alt = np.repeat(alt, num_cols)
+
+    # # num_col = atb.shape[0] # number of rows, say 7903
+    # num_cols = atb.shape[1] # number of cols, say 757
+
+    # # maintain data shape
+    # # delta = np.repeat(delta, num_cols)
+    # # lon = np.repeat(lon, num_cols)
+    # # lat = np.repeat(lat, num_cols)
+    # # alt = np.repeat(alt, num_cols)
 
     
-    atb = atb.flatten()
+    # # atb = atb.flatten()
     
     # time correction.
-    time = (delta - np.datetime64('1970-01-01')).astype('timedelta64[s]').astype(np.int64)
+    # time = (delta - np.datetime64('1970-01-01')).astype('timedelta64[s]').astype(np.int64)
 
     # !!! no lon alt lat correction for now.
     
     # sort data by time
+    
+
     sort_idx = np.argsort(time)
 
     lon = lon[sort_idx]
     lat = lat[sort_idx]
     alt = alt[sort_idx]
-    atb = atb[sort_idx]*10000
+    atb = atb[sort_idx]
     time = time[sort_idx]
 
-    # remove infinite atb value and negative altitude values using mask
-    mask = np.logical_and(np.isfinite(atb), alt > 0) # alt value when not avail is defaulted to -999.0
-    lon = lon[mask]
-    lat = lat[mask]
-    alt = alt[mask]
-    atb = atb[mask]
-    time = time[mask]
+    # # remove infinite atb value and negative altitude values using mask
+    # mask = np.logical_and(np.isfinite(atb), alt > 0) # alt value when not avail is defaulted to -999.0
+    # lon = lon[mask]
+    # lat = lat[mask]
+    # alt = alt[mask]
+    # atb = atb[mask]
+    # time = time[mask]
 
     # Now populate (append) the empty rows with modified data.
     z_location.append(np.stack([lon, lat, alt], axis=-1))
     z_ref.append(atb)
+
     n_time = np.append(n_time, time)
 
     idx = np.arange(0, n_time.size, chunk)
@@ -129,31 +158,33 @@ def data_pre_process(bucket_name, field_campaign, input_data_dir, output_data_di
         keys.append(obj.key)
 
     result = keys
-    for s3_raw_file_key in result:
-        pass
-        # # SOURCE DIR.
-        # sdate = s3_raw_file_key.split("_")[5].split(".")[0]
-        # print(f'processing CRS file {s3_raw_file_key}')
+    # for s3_raw_file_key in result:
 
-        # # CREATE A LOCAL DIR TO HOLD RAW DATA AND CONVERTED DATA
-        # folder = f"/tmp/cpl_olympex/zarr/{sdate}"
-        # point_cloud_folder = f"{folder}/point_cloud"
-        # if os.path.exists(folder): shutil.rmtree(f"{folder}")
-        # # os.mkdir(folder)
-        # Path(folder).mkdir(parents=True, exist_ok=True)
-        # # LOAD FROM SOURCE WITH NECESSARY PRE PROCESSING. CONVERT LEVEL 1B RAW FILES INTO ZARR FILE.
-        # src_s3_path = f"s3://{bucket_name}/{s3_raw_file_key}"
-        # ingest(folder, src_s3_path, bucket_name)
-        # # CONVERT ZARR FILE INTO 3D TILESET JSON.
-        # generate_point_cloud("atb",  0,  1000000000000, folder, point_cloud_folder)
-        # # UPLOAD CONVERTED FILES.
-        # files = os.listdir(point_cloud_folder)
-        # s3 = boto3.client('s3')
-        # for file in files:
-        #     fname = os.path.join(point_cloud_folder, file) # SOURCE
-        #     s3name = f"{field_campaign}/{output_data_dir}/cpl/{sdate}/{file}" # DESTINATION
-        #     print(f"uploaded to {s3name}.")
-        #     upload_to_s3(fname, bucket_name, s3_name=s3name)
+    # # SOURCE DIR.
+    # sdate = s3_raw_file_key.split("_")[5].split(".")[0]
+    # print(f'processing CRS file {s3_raw_file_key}')
+    
+    sdate = "20151203"
+    # CREATE A LOCAL DIR TO HOLD RAW DATA AND CONVERTED DATA
+    folder = f"/tmp/npol_olympex/zarr/{sdate}"
+    point_cloud_folder = f"{folder}/point_cloud"
+    if os.path.exists(folder): shutil.rmtree(f"{folder}")
+    # os.mkdir(folder)
+    Path(folder).mkdir(parents=True, exist_ok=True)
+    # LOAD FROM SOURCE WITH NECESSARY PRE PROCESSING. CONVERT LEVEL 1B RAW FILES INTO ZARR FILE.
+    # src_s3_path = f"s3://{bucket_name}/{s3_raw_file_key}"
+    src_s3_path = "abc"
+    ingest(folder, src_s3_path, bucket_name)
+    # # CONVERT ZARR FILE INTO 3D TILESET JSON.
+    generate_point_cloud("atb",  0,  1000000000000, folder, point_cloud_folder)
+    # # UPLOAD CONVERTED FILES.
+    files = os.listdir(point_cloud_folder)
+    s3 = boto3.client('s3')
+    for file in files:
+        fname = os.path.join(point_cloud_folder, file) # SOURCE
+        s3name = f"{field_campaign}/{output_data_dir}/npol/{sdate}/{file}" # DESTINATION
+        print(f"uploaded to {s3name}.")
+        upload_to_s3(fname, bucket_name, s3_name=s3name)
 
 
 def npol():
